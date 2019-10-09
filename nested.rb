9356483b50b41f9b9d7e93f0e39671e296cab2dd
Michael Z <mziacm@gmail.com>
2019-10-09 21:50:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -4,14 +4,6 @@ def hopper
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
         }
      }
 end
